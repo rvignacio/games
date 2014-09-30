08ac876e132871af249275481cab07ec4bd8f56f
rvignacio <rvignacio@gmail.com>
2014-09-29 23:43:27 -0300

    Heap's algorithm

--- permutations/heap.go
+++ permutations/heap.go
@@ -8,27 +8,27 @@ func swap(values []interface{}, i, j int) {
 
 func Generate(data ...interface{}) (results [][]interface{}) {
 
-	results = append(results, data)
-	result := make([]interface{}, len(data))
-	copy(result, data)
+	tmp := make([]interface{}, len(data))
+	copy(tmp, data)
+	results = append(results, tmp)
 
 	counter := make([]int, len(data))
 
-	for i := 0; i < len(result); {
+	for i := 0; i < len(data); {
 		if counter[i] < i {
 			if i%2 == 0 {
-				swap(result, 0, i)
+				swap(data, 0, i)
 			} else {
-				swap(result, counter[i], i)
+				swap(data, counter[i], i)
 			}
 
 			counter[i] += 1
 
 			i = 0
 
-			tmp := make([]interface{}, len(result))
-			copy(tmp, result)
-			results = append(results, result)
+			tmp = make([]interface{}, len(data))
+			copy(tmp, data)
+			results = append(results, tmp)
 		} else {
 			counter[i] = 0
 			i++
--- permutations/heap_test.go
+++ permutations/heap_test.go
@@ -4,10 +4,45 @@ import (
 	"testing"
 )
 
-func TestHeapPermutation(t *testing.T) {
+func Test2Permutation(t *testing.T) {
 
-	r := Generate("A", "B", "C", "D")
+	r := Generate("A", "B")
 
-	t.Errorf("%d\n%+v", len(r), r)
+	if len(r) != 2 {
+		t.Errorf("Expected 2 permutations, but got %d.", len(r))
+	}
+
+	// Test r[0] == ["A", "B"]
+	if r[0][0] != "A" && r[0][1] != "B" {
+		t.Errorf("Expected first permutation to be [A, B], but got %+v", r[0])
+	}
+
+	// Test r[1] == ["B", "A"]
+	if r[1][0] != "B" && r[1][1] != "A" {
+		t.Errorf("Expected first permutation to be [B, A], but got %+v", r[0])
+	}
+
+}
+
+func TestNPermutation(t *testing.T) {
+
+	var fact func(i int) int
+
+	fact = func(i int) int {
+		if i == 1 {
+			return 1
+		}
+		return i * fact(i-1)
+	}
+
+	values := make([]interface{}, 0)
+
+	for _, v := range []interface{}{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"} {
+		values = append(values, v)
+		r := Generate(values...)
+		if len(r) != fact(len(values)) {
+			t.Errorf("%d", len(r))
+		}
+	}
 
 }
